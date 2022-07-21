5ea559f2d1851a305f8bb74d55e7fa5aadeccdd6
AssassinFlash <857558227@qq.com>
2022-07-21 10:56:56 +0800

    webpack_01

---
+++ 5.webpack/02_webpack-css/src/js/element.js
@@ -0,0 +1,8 @@
+import "../css/style.css"
+import "../css/title.less"
+
+const divEl = document.createElement('div');
+divEl.className = "title"
+divEl.innerHTML = "你好啊，webpack"
+
+document.body.appendChild(divEl)
