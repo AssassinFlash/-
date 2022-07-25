ef7f6901f059a9ba472c42cb9b1c1b8ec91b9163
Flash <857558227@qq.com>
2022-07-26 00:54:46 +0800

    'vue组件化开发01'

---
+++ 7.vue组件化开发/learn_vue_component/src/01_组件的拆分和嵌套/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header></Header>
+  <div class="main">
+    <h2>Banner</h2>
+    <ul>
+      <li>商品信息1</li>
+      <li>商品信息2</li>
+      <li>商品信息3</li>
+      <li>商品信息4</li>
+      <li>商品信息5</li>
+    </ul>
+  </div>
+  <div class="footer">
+    <h2>Footer</h2>
+  </div>
+</template>
+
+<script setup>
+import Header from "./Header.vue";
+</script>
+
+<style scoped>
+
+</style>
